37fb2aa28609654b3b4894c40ff785a688dd10ea
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/DxSample/Form1.vb
@@ -0,0 +1,25 @@
+﻿Imports Microsoft.VisualBasic
+Imports DevExpress.Utils
+Imports DevExpress.XtraGrid.Views.Base
+Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Linq
+Imports System.Text
+Imports System.Windows.Forms
+
+Namespace DxSample
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub OnValidateRow(ByVal sender As Object, ByVal e As ValidateRowEventArgs) Handles gridViewEx1.ValidateRow
+			Dim p As Person = CType(e.Row, Person)
+			e.Valid = String.IsNullOrEmpty(p.Name) OrElse p.Name.Contains("A")
+		End Sub
+	End Class
+End Namespace
---
+++ VB/DxSample/Person.vb
@@ -0,0 +1,16 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.ComponentModel
+
+Namespace DxSample
+	Public Class Person
+		Private privateName As String
+		Public Property Name() As String
+			Get
+				Return privateName
+			End Get
+			Set(ByVal value As String)
+				privateName = value
+			End Set
+		End Property
+	End Class
+End Namespace
